2f88b72d9b33823a5560245941127642aaf900ec
yanxuegan <yanxuegan0903@163.com>
2017-07-13 10:29:38 +0800

    Initial Commit

---
+++ AudioPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioPlayer
+//
+//  Created by vsKing on 2017/7/13.
+//  Copyright © 2017年 vsKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
