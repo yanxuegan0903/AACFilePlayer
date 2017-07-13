b32bc9c76b3e4d0d5f66829f9a58e6e7bb7e5a68
yanxuegan <yanxuegan0903@163.com>
2017-07-13 14:45:56 +0800

    可以播放ACC音频文件

--- AudioPlayer/ViewController.m
+++ AudioPlayer/ViewController.m
@@ -8,16 +8,85 @@
 
 #import "ViewController.h"
 
+#import "PlayAudio.h"
+
+
 @interface ViewController ()
+
+@property (nonatomic, strong) PlayAudio *playAudio;
 
 @end
 
 @implementation ViewController
 
+- (PlayAudio *)playAudio{
+    if (!_playAudio) {
+        _playAudio = [[PlayAudio alloc] init];
+    }
+    return _playAudio;
+}
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"123");
+    
+    
+    UIButton * start = [[UIButton alloc] initWithFrame:CGRectMake(50, 100, 100, 60)];
+    [start setTitle:@"开始" forState:UIControlStateNormal];
+    [start setBackgroundColor:[UIColor greenColor]];
+    [self.view addSubview:start];
+    [start addTarget:self action:@selector(start) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
+    
+    UIButton * stop = [[UIButton alloc] initWithFrame:CGRectMake(50, 170, 100, 60)];
+    [stop setTitle:@"停止" forState:UIControlStateNormal];
+    [stop setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:stop];
+    [stop addTarget:self action:@selector(stop) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+
+- (void) start{
+    
+    NSLog(@"点击 start");
+    
+    NSString * path = [[NSBundle mainBundle] pathForResource:@"testfile" ofType:@"aac"];
+    
+    if (path) {
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            [self.playAudio initWithAudio:path];
+        });
+        
+        
+    }else{
+        NSLog(@"path 不存在");
+    }
+}
+
+
+- (void)stop{
+    NSLog(@"点击 stop");
+    [self.playAudio stop];
+}
+
+
 
 
 - (void)didReceiveMemoryWarning {
